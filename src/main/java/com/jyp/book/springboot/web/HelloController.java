6b5ecf817b56318c6d7e8409cf4ff51a70cf459f
JYPs <qkrwndma5@naver.com>
2020-05-30 19:37:39 +0900

    롬복추가, gradle 버전 번경(6.x -> 4.x), 테스트 코드 추가

--- src/main/java/com/jyp/book/springboot/web/HelloController.java
+++ src/main/java/com/jyp/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.jyp.book.springboot.web;
 
+import com.jyp.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +11,16 @@ public class HelloController {
     public String hello(){
         return "hello";
     }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, // 1)
+                                     @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
 }
+
+/*1)
+* ReqeustParam
+*   외부에서 API로 넘긴 파라미터를 가져오는 어노테이션
+*   외부에서 name (@RequestParam("name")) 이란 이름으로 넘긴 파라미터를 메소드 파라미터 name(String name)에 저장하게 된다
+* */
--- src/test/java/com/jyp/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jyp/book/springboot/web/HelloControllerTest.java
@@ -1,4 +1,4 @@
-package com.jyp.book.springboot;
+package com.jyp.book.springboot.web;
 
 import com.jyp.book.springboot.web.HelloController;
 import org.junit.Test;
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class) // 1)
@@ -19,14 +22,30 @@ public class HelloControllerTest {
     private MockMvc mvc; // 4)
 
     @Test
-    public  void  hello가_리턴된다() throws Exception{
+    public void hello가_리턴된다() throws Exception {
         String hello = "hello";
 
         mvc.perform(get("/hello")) // 5)
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
     }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                    get("/hello/dto")
+                            .param("name", name) //6)
+                            .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name))) //7)
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+
 }
+
 /* 1)
  * RunWith ==> 테스트를 진행할 때 Junit에 내장된 실행자 외에 다른 실행자를 실행, 스프링 부트 테스트와 JUnit 사이에 연결자 역할!
  * SpringRunner.class ==> 다른 실행자
@@ -63,3 +82,13 @@ public class HelloControllerTest {
  * 응답 본문의 내용을 검증
  * Controller에서 "hello"를 리턴하기 때문에 이 값이 맞는지 검증
  * */
+
+/*6)
+* param ==> api 테스틀할 때 사용될 요청 파리미터를 설정
+* 단, 값은 String만 허용
+* 숫자/날짜 등의 데이터는 등록할 때 문자열로 변경해야만 가능
+*
+* 7)
+* jsonPath ==> JSON 응답값을 필드별로 검증할 수 있는 메소드
+* $를 기준으로 필드명을 명시
+* */
