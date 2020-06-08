805137afe6b23324d7daf16a9dd1de1d64cd3465
JYPs <qkrwndma5@naver.com>
2020-06-08 22:36:41 +0900

    게시글 수정

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -4,6 +4,9 @@ var main = {
         $('#btn-save').on('click', function () {
             _this.save();
         });
+        $('#btn-update').on('click', function () { // btn-update란 id를 가진 HTML 엘리먼트에 click 이벤트가 발생할 때 update function을 실행하도록 이벤트를 등록
+            _this.update();
+        });
     },
     save : function () {
         var data = {
@@ -24,8 +27,35 @@ var main = {
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
-    }
+    },
+    update : function () { // 신규로 추가될 update function임
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
 
+        $.ajax({
+            type: 'PUT', // 1)
+            url: '/api/v1/posts' + id, // 어느 게시글을 수정할지 URL Path로 구분하기 위해 Path에 id를 추가한다
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            window.location.href = '/'; // 글 수정되면 메인페이지(/)로 이동한다
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    }
 };
 
 main.init();
+/*
+    1) 여러 HTTP Method 중 PUT 메소드를 선택
+    PostsApiController에 있는 API에서 이미 @PutMapping으로 선언했기 때문에 PUT을 사용해야 한다
+    REST에서 CRUD는 다음과 같이 HTTP Method에 매핑된다
+        -생성(Create) : POST
+        -읽기(Read) : GET
+        -수정(Update) : PUT
+        -삭제(Delete) : DELETE
+*/
