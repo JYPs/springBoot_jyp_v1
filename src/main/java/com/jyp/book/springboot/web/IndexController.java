a88a9392e9dfc1c642f5f1f481c6119e50d2745e
JYPs <qkrwndma5@naver.com>
2020-06-15 21:40:32 +0900

    로그인 테스트까지 완료~~

--- src/main/java/com/jyp/book/springboot/web/IndexController.java
+++ src/main/java/com/jyp/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jyp.book.springboot.web;
 
+import com.jyp.book.springboot.config.auth.dto.SessionUser;
 import com.jyp.book.springboot.service.posts.PostsService;
 import com.jyp.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
@@ -25,16 +28,32 @@ public class IndexController {
     * */
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) { //1)
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user"); //2)
+
+        if (user != null) { //3)
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
     /*1)Model
      * 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다
      * postsService.findAllDesc()로 가져온 결과를 posts로 index.mustache에 전달한다
      * */
+    /*
+    * 2)(SessionUser) httpSession.getAttribute("user")
+    *   앞서 작성된 CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 구성
+    *   즉, 로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수 있다
+    *
+    * 3)if (user != null)
+    *   세션에 저장된 값이 있을 때만 model에 userName으로 등록
+    *   세션에 저장된 값이 없으면 model엔 아무런 값이 없는 상태이니 로그인 버튼이 보이게 된다.
+    * */
 
     @GetMapping("/posts/save")
     public String postsSave() {
